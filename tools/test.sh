4f29e00ff9da6534753e4b9f63fc6cd2ba03678f
Robert Langlois <ezralanglois@gmail.com>
2017-04-12 20:15:43 -0700

    Add script to build InterOp in a centos5 docker image

---
+++ tools/test.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -ex
+
+INTEROP_C89=OFF
+BUILD_TYPE=Release
+
+if [ ! -z $1 ] ; then
+    INTEROP_C89=$1
+fi
+
+if [ ! -z $2 ] ; then
+    BUILD_TYPE=$2
+fi
+
+if [[ "$3" == "travis" ]]; then
+    TRAVIS_BEG='travis_fold:start:script.1\\r'
+    TRAVIS_END='travis_fold:end:script.1\\r'
+else
+    TRAVIS_BEG=""
+    TRAVIS_END=""
+fi
+
+if [ -e build ]; then
+    rm -fr build
+fi
+
+mkdir build
+
+echo 'Configuring...' && echo -en ${TRAVIS_BEG}
+cmake -H. -Bbuild -DNUNIT_ROOT=$HOME/NUnit-2.6.4 -DENABLE_BACKWARDS_COMPATIBILITY=$INTEROP_C89 -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=./usr
+echo -en ${TRAVIS_END}
+cmake --build build
+echo 'Installing...' && echo -en ${TRAVIS_BEG}
+cmake --build build --target install
+echo -en ${TRAVIS_END}
+cmake --build build --target check
+echo 'Cleaning...' && echo -en ${TRAVIS_BEG}
+cmake --build build --target clean
+echo -en ${TRAVIS_END}
